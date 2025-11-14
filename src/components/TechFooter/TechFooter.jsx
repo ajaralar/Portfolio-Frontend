10487fe29147f45dcd35e8fe38dabbe2abaef0c6
Aj-Aralar <ajamyaralar@gmail.com>
2025-11-14 18:25:47 -0500

    build(footer):
    * Add social links
    * Add dynamic footerData for email and phone

--- src/components/TechFooter/TechFooter.jsx
+++ src/components/TechFooter/TechFooter.jsx
@@ -2,11 +2,35 @@ import classes from "./TechFooter.module.css";
 
 const TechFooter = ({ footerData }) => {
   return (
-    <>
-      <footer>
-        {footerData.copyright}
-      </footer>
-    </>
+    <footer>
+      <div className={classes.mainContainer}>
+
+        <div className={classes.imageSection}>
+
+        </div>
+
+        <div className={classes.section}>
+
+          <div className={classes.containerOne}>
+            <p className={classes.title}>INQUIRIES? <span className={classes.titleSpan}>GET IN TOUCH</span></p>
+            <p className={classes.details}>{footerData.email}</p>
+            <p className={classes.details}>{footerData.phone}</p>
+          </div>
+
+          <div className={classes.containerTwo}>
+            <p className={classes.body}>{footerData.body}</p>
+            <div className={classes.divider}></div>
+            <div className={classes.socialsContainer}>
+              {footerData.socialLinks.map((link, index) => (
+                <a href={link.url} key={index} className={classes.link}>{link.name}</a>
+              ))}
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </footer>
   );
 };
 
