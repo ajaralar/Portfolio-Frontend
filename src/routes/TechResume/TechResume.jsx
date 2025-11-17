943ad964b1c87e6aa7306d512326570e2fd6943f
Aj-Aralar <ajamyaralar@gmail.com>
2025-11-17 16:01:08 -0500

    build(resume):
    * Create PIICard component
    * Finished text typing animation logic, Just have to make it into a component

--- src/routes/TechResume/TechResume.jsx
+++ src/routes/TechResume/TechResume.jsx
@@ -1,33 +1,23 @@
 import { useLoaderData } from 'react-router-dom'
 import classes from './TechResume.module.css'
-import Card from '../../components/Card/Card'
+import PIICard from '../../components/PIICard/PIICard'
 
 const TechResume = () => {
     const data = useLoaderData()
     return (
         <div className={classes.resumeContainer}>
             <div className={classes.sectionOne}>
-                <Card>
-                    <div className={classes.piiCard}>
-                        <div className={classes.pii}>
-                            <div className={classes.image}>
-                                {/* <img src="" alt="" /> */}
-                            </div>
-                            <p>JOSE RICKY, ARALAR</p>
-                            <p>Software Developer</p>
-                        </div>
-                    </div>
-                </Card>
-                <Card>
+                <PIICard data={data}></PIICard>
+                {/* <Card>
                     <div className={classes.skillsCard}>skillsCard</div>
-                </Card>
+                </Card> */}
             </div>
 
-            <div className={classes.sectionTwo}>
+            {/* <div className={classes.sectionTwo}>
                 <Card>
                     <div className={classes.careerCard}>careerCard</div>
                 </Card>
-            </div>
+            </div> */}
         </div>
     )
 }
