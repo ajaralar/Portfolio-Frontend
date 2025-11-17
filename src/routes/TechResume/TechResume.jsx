7f7af4a10f4a3c33a67a8380879c445cd19d878f
Aj-Aralar <ajamyaralar@gmail.com>
2025-11-17 12:44:47 -0500

    build(resume):
    * Set global styles
    * Refactor multiple module.css files
    * Create TechResume.module.css
    * Work on unfinished TechResume.jsx

--- src/routes/TechResume/TechResume.jsx
+++ src/routes/TechResume/TechResume.jsx
@@ -1,13 +1,36 @@
 import { useLoaderData } from 'react-router-dom'
+import classes from './TechResume.module.css'
+import Card from '../../components/Card/Card'
 
 const TechResume = () => {
     const data = useLoaderData()
     return (
-        <>
-            <p>Hello from Resume page</p>
-            <p>Status: {data.message} </p>
-        </>
+        <div className={classes.resumeContainer}>
+            <div className={classes.sectionOne}>
+                <Card>
+                    <div className={classes.piiCard}>
+                        <div className={classes.pii}>
+                            <div className={classes.image}>
+                                {/* <img src="" alt="" /> */}
+                            </div>
+                            <p>JOSE RICKY, ARALAR</p>
+                            <p>Software Developer</p>
+                        </div>
+                    </div>
+                </Card>
+                <Card>
+                    <div className={classes.skillsCard}>skillsCard</div>
+                </Card>
+            </div>
+
+            <div className={classes.sectionTwo}>
+                <Card>
+                    <div className={classes.careerCard}>careerCard</div>
+                </Card>
+            </div>
+        </div>
     )
 }
 
 export default TechResume
+
