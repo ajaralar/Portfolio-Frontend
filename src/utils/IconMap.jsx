11b132a0b846a60ad3a12e5f16ea5760400da0c5
Aj-Aralar <ajamyaralar@gmail.com>
2025-11-19 17:25:48 -0500

    build(resume):
    * Create 3 Icon components (Html, Css, JavaScript)
    * Add new icons to IconMap.jsx

--- src/utils/IconMap.jsx
+++ src/utils/IconMap.jsx
@@ -2,10 +2,16 @@ import CodeIcon from '../components/Icons/CodeIcon/CodeIcon';
 import EditIcon from '../components/Icons/EditIcon/EditIcon';
 import LearnIcon from '../components/Icons/LearnIcon/LearnIcon';
 import AWSIcon from '../components/Icons/AWSIcon/AWSIcon';
+import HtmlIcon from '../components/Icons/HtmlIcon/HtmlIcon';
+import CssIcon from '../components/Icons/CssIcon/CssIcon';
+import JavaScriptIcon from '../components/Icons/JavaScriptIcon/JavaScriptIcon';
 
 export const IconMap = {
     code: CodeIcon,
     design: EditIcon,
     learn: LearnIcon,
     aws: AWSIcon,
+    html: HtmlIcon,
+    css: CssIcon,
+    javascript: JavaScriptIcon,
 };
