fb563274900a2b7091e7d78f9a249cc4dc01d4ac
Aj-Aralar <ajamyaralar@gmail.com>
2025-11-22 17:12:12 -0500

    build(resume): Refactor AWSIcon to CloudIcon

--- src/utils/IconMap.jsx
+++ src/utils/IconMap.jsx
@@ -1,11 +1,11 @@
 import CodeIcon from '../components/Icons/CodeIcon/CodeIcon';
 import EditIcon from '../components/Icons/EditIcon/EditIcon';
 import LearnIcon from '../components/Icons/LearnIcon/LearnIcon';
-import AWSIcon from '../components/Icons/AWSIcon/AWSIcon';
+import CloudIcon from '../components/Icons/CloudIcon/CloudIcon';
 
 export const IconMap = {
     code: CodeIcon,
     design: EditIcon,
     learn: LearnIcon,
-    aws: AWSIcon,
+    cloud: CloudIcon,
 };
