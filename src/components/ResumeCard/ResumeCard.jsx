5304bce52deb29a9fadf286e08046128f88cf1db
Aj-Aralar <ajamyaralar@gmail.com>
2025-11-23 22:16:38 -0500

    build(resume):
    * Create Icon components for ResumeCard component
    * Create new Icon map for ResumeCard component
    * Finish headers for ResumeCard component

--- src/components/ResumeCard/ResumeCard.jsx
+++ src/components/ResumeCard/ResumeCard.jsx
@@ -1,29 +1,40 @@
 import classes from './ResumeCard.module.css'
+import { ResumeIconMap } from '../../utils/ResumeIconMap'
 import Card from "../Card/Card"
 
 const ResumeCard = ({ data }) => {
-    const education = data.education
     return (
         <Card>
             <div className={classes.mainContainer}>
+                {data.collections.map((collection, index) => {
+                    const Icon = ResumeIconMap[collection.icon]
 
-                <div className={classes.collectionHeader}>
-                    <div className={classes.iconContainer}>
-                        {/* Icon here */}
-                    </div>
-                    <p className={classes.title}>Education</p>
-                </div>
+                    return (
+                        <>
+                            <div key={index} className={classes.collectionHeader}>
+                                <div className={classes.iconContainer}>
+                                    <Icon />
+                                </div>
 
-                <div className="itemCollection">
-                    <div className="item">
-                        <div className="timelineGraphic"></div>
-                        <div className="content">
-                            <p>School / University</p>
-                            <p>Date</p>
-                            <p>Body</p>
-                        </div>
-                    </div>
-                </div>
+                                <p className={classes.title}>{collection.collectionHeader}</p>
+                            </div>
+
+                            <div className="itemCollection">
+                                <div className="item">
+                                    <div className="timelineGraphic"></div>
+                                    <div className="content">
+                                        <p>School / University</p>
+                                        <p>Date</p>
+                                        <p>Body</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+
+                    )
+                })}
+
+
 
             </div>
         </Card>
---
+++ src/utils/ResumeIconMap.jsx
@@ -0,0 +1,11 @@
+import HatIcon from '../components/Icons/HatIcon/HatIcon'
+import CaseIcon from '../components/Icons/CaseIcon/CaseIcon'
+import IdeaIcon from '../components/Icons/IdeaIcon/IdeaIcon'
+import VerifiedIcon from '../components/Icons/VerifiedIcon/VerifiedIcon'
+
+export const ResumeIconMap = {
+    hat: HatIcon,
+    case: CaseIcon,
+    idea: IdeaIcon,
+    verified: VerifiedIcon,
+}
