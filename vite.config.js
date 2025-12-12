daab0e8ec6c070988973a3f3050cd628f9e01c47
Aj-Aralar <ajamyaralar@gmail.com>
2025-12-12 10:04:30 -0500

    build(test): Configure to test on mobile

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This tells Vite to listen on all network interfaces
+    port: 5173,     // Ensure the port is correct
+  }
 })
