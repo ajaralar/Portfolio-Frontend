c041f6d140d965698e6112bf0f5aa913e7413e31
Aj-Aralar <ajorick.aralar@gmail.com>
2026-01-14 21:32:24 -0500

    build(home): Load data from backend

--- src/routes/HomeLayout/HomeLayout.jsx
+++ src/routes/HomeLayout/HomeLayout.jsx
@@ -1,29 +1,34 @@
-import { Link } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 import classes from './HomeLayout.module.css'
 
 const HomeLayout = () => {
+    const data = useLoaderData()
+
     return (
         <main>
             <div className={classes.mainContainer}>
                 <h1 className={classes.title}>CHOOSE A PORTFOLIO</h1>
 
                 <div className={classes.portalGrid}>
-                    <Link to="/tech" className={`${classes.card} ${classes.techCard}`}>
-                        <div className={classes.overlay}>
-                            <h2>Tech Portfolio</h2>
-                            <p>Full-Stack Development & Cloud Architecture</p>
-                        </div>
-                    </Link>
+                    {data.portfolios.map((portfolio, index) => {
+                        const url = portfolio.url
+                        return (
+                            <Link
+                                to={url}
+                                key={index}
+                                className={`${classes.card} ${classes.techCard}`}>
 
-                    <Link to="/music" className={`${classes.card} ${classes.musicCard}`}>
-                        <div className={classes.overlay}>
-                            <h2>Music Portfolio</h2>
-                            <p>Production, Performance & Composition</p>
-                        </div>
-                    </Link>
+                                <div className={classes.overlay}>
+                                    <h2>{portfolio.title} Portfolio</h2>
+                                    <p>{portfolio.subtitle}</p>
+                                </div>
+
+                            </Link>
+                        )
+                    })}
                 </div>
             </div>
-        </main>
+        </main >
     )
 }
 
